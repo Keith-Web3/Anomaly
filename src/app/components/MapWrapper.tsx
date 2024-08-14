ff3b9c08faf6e3c5970e4222f66dd71052b0f678
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-08-14 10:03:48 +0100

    feat: maps/satellite switch

--- src/app/components/MapWrapper.tsx
+++ src/app/components/MapWrapper.tsx
@@ -1,29 +1,97 @@
 'use client'
-import * as React from 'react'
+import { useState, useEffect } from 'react'
 import Map, { Marker } from 'react-map-gl'
+import { twMerge } from 'tailwind-merge'
+import { Roboto } from 'next/font/google'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './mapWrapper.css'
 
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN // Set your mapbox token here
-
-console.log(MAPBOX_TOKEN)
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['500', '400'],
+})
 
 export default function MapWrapper() {
+  const [mapStyle, setMapStyle] = useState<'streets' | 'satellite'>('streets')
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number
+    longitude: number
+  }>({ latitude: 0, longitude: 0 })
+
+  useEffect(() => {
+    // Check if the Geolocation API is supported
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords
+          setUserLocation({ latitude, longitude })
+          console.log(latitude, longitude)
+        },
+        error => {
+          console.error('Error fetching geolocation: ', error)
+        }
+      )
+    } else {
+      console.error('Geolocation is not supported by this browser.')
+    }
+  }, [])
+
   return (
-    <div className="grid min-h-screen">
-      <Map
-        initialViewState={{
-          latitude: 37.8,
-          longitude: -122.4,
-          zoom: 14,
-        }}
-        style={{ width: '100%', height: '100%' }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={MAPBOX_TOKEN}
+    <>
+      <div
+        className={twMerge(
+          'flex items-center w-max mx-auto rounded fixed inset-[1em_50%_auto_auto] z-[1] overflow-hidden translate-x-1/2 shadow-[0_1.5px_4.4px_#00000033] bg-white',
+          roboto.className
+        )}
       >
-        <Marker longitude={-122.4} latitude={37.8} color="red" />
-      </Map>
-    </div>
+        <button
+          className={twMerge(
+            'outline-none cursor-pointer bg-white px-5 py-3 text-[#565656] capitalize border-r-2 border-[#F2F2F2]',
+            mapStyle === 'streets' && 'font-medium text-black'
+          )}
+          onClick={() => setMapStyle('streets')}
+        >
+          map
+        </button>
+        <button
+          className={twMerge(
+            'border-none outline-none cursor-pointer bg-white px-5 py-3 text-[#565656] capitalize',
+            mapStyle === 'satellite' && 'font-medium text-black'
+          )}
+          onClick={() => setMapStyle('satellite')}
+        >
+          satellite
+        </button>
+      </div>
+      <div className="grid min-h-screen">
+        {!!userLocation.latitude && !!userLocation.longitude && (
+          <Map
+            initialViewState={{
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude,
+              zoom: 14,
+            }}
+            style={{ width: '100%', height: '100%' }}
+            mapStyle={
+              mapStyle === 'streets'
+                ? 'mapbox://styles/mapbox/streets-v11'
+                : 'mapbox://styles/mapbox/satellite-streets-v12'
+            }
+            mapboxAccessToken={MAPBOX_TOKEN}
+            onClick={e => {
+              console.log(e)
+            }}
+          >
+            <Marker
+              longitude={userLocation.longitude}
+              latitude={userLocation.latitude}
+              color="red"
+            />
+          </Map>
+        )}
+      </div>
+    </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import MapWrapper from './components/MapWrapper'
 export default function Home() {
   return (
     <div>
-      <p>Hello world</p>
       <MapWrapper />
     </div>
   )
