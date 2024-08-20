4b8e18a53e584fe4f38e4f3914e32656574756a4
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-08-20 15:21:42 +0100

    feat: toggle password visibility

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,12 +1,18 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
-import roadImg from '../../assets/road.svg'
-import googleImg from '../../assets/google.svg'
-import eyeImg from '../../assets/eye.svg'
+
+import roadImg from '@/assets/road.svg'
+import googleImg from '@/assets/google.svg'
+import eyeImg from '@/assets/eye.svg'
+import { signup } from '@/actions'
 
 export default function SignUp() {
+  const [isPasswordDisplayed, setIsPasswordDisplayed] = useState<boolean>(false)
   return (
     <div className="min-h-screen p-4 grid place-content-center">
-      <form className="w-max min-w-[350px]">
+      <form className="w-max min-w-[350px]" action={signup}>
         <div>
           <Image src={roadImg} alt="road" className="mx-auto" />
           <p className="text-[#831DD3] text-center capitalize text-lg font-bold">
@@ -28,6 +34,7 @@ export default function SignUp() {
         <p className="text-center text-[#969696] text-sm my-4">or</p>
         <input
           type="email"
+          name="email"
           placeholder="Email address"
           required
           className="border border-[#E5E5E5] rounded-lg outline-none placeholder:text-[#747474] w-full mb-4 py-3 px-5"
@@ -37,7 +44,7 @@ export default function SignUp() {
           className="flex items-center border border-[#E5E5E5] rounded-lg outline-none w-full py-3 px-5"
         >
           <input
-            type="password"
+            type={isPasswordDisplayed ? 'text' : 'password'}
             name="password"
             id="password"
             placeholder="Password"
@@ -45,7 +52,12 @@ export default function SignUp() {
             required
             className="border-none outline-none w-full placeholder:text-[#747474]"
           />
-          <Image src={eyeImg} alt="eye" className="cursor-pointer" />
+          <Image
+            src={eyeImg}
+            alt="eye"
+            className="cursor-pointer"
+            onClick={() => setIsPasswordDisplayed(prev => !prev)}
+          />
         </label>
         <button className="text-center bg-[#831DD3] border-none outline-none cursor-pointer w-full px-5 py-3 mt-4 rounded-lg text-white capitalize font-medium">
           create account
