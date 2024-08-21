dde13314e77d8d72e797029869a8cbd78bb672ea
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-08-21 16:53:57 +0100

    chore: setup react-hot-toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { twMerge } from 'tailwind-merge'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,25 @@ export default function RootLayout({
     <html lang="en">
       <body className={twMerge('min-h-screen', inter.className)}>
         {children}
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: '16px',
+              maxWidth: 'max-content',
+              padding: '16px 24px',
+              color: '#333333',
+            },
+          }}
+        />
       </body>
     </html>
   )
